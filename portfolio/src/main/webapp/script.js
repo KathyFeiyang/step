9eec94ec97c9535b108d2d9c261759e6e8427576
Feiyang Yu <kathyyufeiyang@gmail.com>
2020-05-27 18:02:58 -0400

    Add dynamic content to homepage: cycling greetings and receipt pop-up for user feedback submission.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,44 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// use modern JavaScript (ES5)
+"use strict"
+
 /**
- * Adds a random greeting to the page.
+ * Adds a cycling greeting to the page.
  */
+let greetingIndex = 0;
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!', 'Hallo Welt!'];
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  // Pick the next greeting in a cycle.
+  const greeting = greetings[greetingIndex];
+
+  // Update greeting index.
+  greetingIndex = (greetingIndex + 1) % greetings.length;
 
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
+}
+
+/**
+ * Presents a receipt for getting user form feedback in pop-up window.
+ */
+function presentFeedbackReceipt() {
+  // Obtain user input feedback content, name and email.
+  const formElements = document.getElementById('feedback-form').elements;
+  const userFeedback = formElements[0].value;
+  const userName = formElements[1].value;
+  const userEmail = formElements[2].value;
+
+  // Construct feedback receipt.
+  const receipt = `Dear ${userName},\nThank you for submitting a feedback!\n`
+                   + `We have recorded the following:\n`
+                   + `    *Message: "${userFeedback}"\n`
+                   + `    *Contact Information: ${userEmail}\n`;
+
+  // Present feedback receipt in pop-up window.
+  window.alert(receipt);
 }
